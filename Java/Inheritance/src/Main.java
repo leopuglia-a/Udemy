cf462b9cd0a1d96019511a33aeaef44ddc8401e3
Leonardo Puglia <leopuglia.a@gmail.com>
2019-05-28 23:11:15 -0300

    inheritance finished

--- Java/Inheritance/src/Main.java
+++ Java/Inheritance/src/Main.java
@@ -1,11 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        Animal animal = new Animal("Animal", 1, 1, 5, 5);
+//        Animal animal = new Animal("Animal", 1, 1, 5, 5);
+//
+//        Dog dog = new Dog("Yorkee", 8, 20, 2, 4, 1, 20, "Long silky");
+//
+//        dog.eat();
+////        dog.walk();
+//        dog.run();
 
-        Dog dog = new Dog("Yorkee", 8, 20, 2, 4, 1, 20, "Long silky");
+//        Challenge
+//        Start with a base class of Vehicle, then create a Car class that inherits from its base class.
+//        Finally, create another class, a specific type of Car that inherits from the car class.
+//        You should be able to hand steering, changing gears, and moving (speed in other words).
+//        You will want to decide where to put the appropriate state and behaviours (fields and methods).
+//        As mentioned above, changing gears, increasing/decreasing speed should be included.
+//        For you specific type of vehicle you will want to add something specific for that type of car.
 
-        dog.eat();
-//        dog.walk();
-        dog.run();
+        Outlander outlander = new Outlander(36);
+        outlander.steer(45);
+        outlander.accelerate(30);
+        outlander.accelerate(20);
+        outlander.accelerate(-42);
+
     }
 }
